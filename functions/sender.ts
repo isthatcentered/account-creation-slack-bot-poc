42c6bccee4e7bb7d7fe13bdda835ae47489b2f21
Edouard Penin <e.peninb@gmail.com>
2019-02-24 10:08:05 +0100

    Full flow working with ugly code

--- functions/sender.ts
+++ functions/sender.ts
@@ -1,11 +1,250 @@
 import { APIGatewayEvent, Callback, Context } from "aws-lambda"
 import { config } from "dotenv"
 import { parse } from "query-string"
+import { LogLevel, WebClient } from "@slack/client"
 
 
 
 
 config()
+
+
+
+const handler = ( event: APIGatewayEvent, context: Context, callback: Callback ): Promise<Response> => {
+	
+	const action: SlackUserAction<{ tool: string, email: string, team_name: string, slack_user: string }> = _extractFormData( event.body ),
+	      client                                                                                          = new WebClient( process.env.SLACK_TOKEN, { logLevel: LogLevel.DEBUG } )
+	
+	let pending: Promise<unknown>
+	
+	console.log( action )
+	
+	const instantiator = action.user.id
+	
+	switch ( action.type ) {
+		
+		case "message_action":
+			
+			pending = (action.message as botMessageInfos).subtype ?// it's a bot message, we can't create accounts for those
+			          cancel() :
+			          _sendDialog( action )
+			break;
+		
+		case "dialog_submission":
+			const { slack_user: asker, email, team_name, tool } = action.submission
+			
+			const sendCredentialsToUser = client.chat.postMessage( {
+				channel:    asker,
+				// Will only appear in notification as we're using blocks
+				text:       `${tool} account created `,
+				// icon_emoji: ":raised_hands:",
+				blocks:     [
+					{
+						type:      "section",
+						text:      {
+							type: "mrkdwn",
+							text: `Hey ${asker}, your ${tool} account has been created, here are your credentials [++ SEND A POLL ++]`,
+						},
+						accessory: {
+							type:      "image",
+							image_url: "https://media.giphy.com/media/wAxlCmeX1ri1y/giphy.gif",
+							alt_text:  "Victory",
+						},
+					},
+				],
+				// as_user:  true, // I don't receive notifications if i send myself a dm 😅
+				// username: "isthatcentered",
+			} )
+			
+			const sendActionSuccessConfirmationToInstantiator = client.chat.postEphemeral( {
+				channel: action.channel.id,
+				user:    action.user.id,
+				text:    "Success, account has been created and sent",
+			} )
+			
+			// const sendUserInfosToInstantiator = client.users.info( { user: asker } )
+			// 	.then( ( { user }: any ) => client.chat.postMessage( {
+			// 			channel: instantiator,
+			// 			text:    `Here's what we got on this guy: name: ${user.name}`,
+			// 		} ),
+			// 	)
+			
+			pending = Promise.all( [
+				sendCredentialsToUser,
+				sendActionSuccessConfirmationToInstantiator, // yeah yeah this can arrive before but we're faking it so it's ok
+				// sendUserInfosToInstantiator,
+			] )
+			break;
+		
+		default:
+			pending = Promise.resolve()
+			break;
+	}
+	
+	
+	// Is it dialog submit
+	// -> Send me the user's infos (this is for demo)
+	//    -> Find out who triggered the actions
+	//    -> Get his infos
+	//    -> Post message to his id
+	// -> send dm
+	//    -> "Hey dude, your account has bee created"
+	//    -> gif
+	// -> Track creation
+	// Send poll
+	
+	
+	return pending
+		.then( res => ({
+			statusCode: 200,
+			body:       "",
+		}) )
+	
+	
+	function _extractFormData( body: Maybe<string> )
+	{
+		const { payload } = (parse( body || "payload=" ) as { payload: string })
+		
+		return JSON.parse( payload ) // payload return a json string even after parse
+	}
+	
+	
+	function cancel(): Promise<any>
+	{
+		return Promise.resolve()
+	}
+	
+	
+	function _sendDialog( action: DialogTriggeredAction )
+	{
+		return client.dialog.open( {
+			trigger_id: action.trigger_id,
+			dialog:     {
+				title:            "Create account",
+				callback_id:      Math.random().toString(), // An identifier strictly for you to recognize submissions of this particular instance of a dialog. Use something meaningful to your app. 255 characters maximum. Don't use this ID to reference sensitive data; use the more expansive state parameter
+				notify_on_cancel: false, // 	Default is false. When set to true, we'll notify your request URL whenever there's a user-induced dialog cancellation
+				submit_label:     "Create",
+				state:            "", // 	An optional string that will be echoed back to your app when a user interacts with your dialog. Use it as a pointer to reference sensitive data stored elsewhere.
+				elements:         [
+					{
+						label:   "Tool",
+						type:    "select",
+						name:    "tool",
+						options: [
+							{
+								label: "Github",
+								value: "github",
+							},
+							{
+								label: "Gitlab",
+								value: "gitlab",
+							},
+							{
+								label: "Artifactory",
+								value: "artifactory",
+							},
+							{
+								label: "Vault",
+								value: "cault",
+							},
+							{
+								label: "Jira",
+								value: "jira",
+							},
+						],
+					},
+					{
+						label:       "Email address",
+						name:        "email",
+						type:        "text",
+						subtype:     "email",
+						placeholder: "you@example.com",
+					},
+					{
+						label:       "Team name",
+						name:        "team_name",
+						type:        "text",
+						placeholder: "software-factory",
+					},
+					{
+						label: "Slack user",
+						name:  "slack_user",
+						type:  "text",
+						value: (action.message as userMessageInfos).user,
+					},
+				],
+			},
+		} )
+		
+	}
+}
+
+export type SlackUserAction<T> = DialogTriggeredAction | DialogSubmitedAction<T>
+
+// see https://api.slack.com/actions
+export interface UserTriggeredAction
+{
+	type: string // Provided by slack, identifies the incomming action
+	token: string
+	action_ts: string
+	callback_id: string // you defined this in app settings https://api.slack.com/apps/AGG0QS9PZ/interactive-messages?
+	// The user who clicked on the action to trigger this request
+	user: {
+		name: string
+		id: string
+	}
+	// Team the source message was in
+	team: {
+		domain: string
+		id: string
+	}
+	// Channel the source message was in
+	channel: {
+		name: string
+		id: string
+	}
+	response_url: string // If you want to respond to the action with a message after receiving the payload
+}
+
+export interface DialogTriggeredAction extends UserTriggeredAction
+{
+	type: "message_action" // provided by slack
+	callback_id: "account_new" // you defined this in app settings https://api.slack.com/apps/AGG0QS9PZ/interactive-messages?
+	trigger_id: string
+	message_ts: string
+	// The message that the user initiated the action with.
+	message: botMessageInfos | userMessageInfos
+}
+
+export interface messageInfos
+{
+	type: "message",
+	text: string,
+	ts: string,
+}
+
+export interface botMessageInfos extends messageInfos
+{
+	subtype: "bot_message",
+	username: string,
+	bot_id: string,
+}
+
+export interface userMessageInfos extends messageInfos
+{
+	client_msg_id: string,
+	user: string,
+}
+
+
+
+export interface DialogSubmitedAction<T>  extends UserTriggeredAction
+{
+	type: "dialog_submission",
+	submission: T,
+	state: string
+}
+
 
 export type Maybe<T> = T | null | undefined
 
@@ -17,261 +256,5 @@ interface Response
 	body?: string
 }
 
-
-const handler = ( event: APIGatewayEvent, context: Context, callback: Callback ): Promise<Response> => {
-	
-	console.log( "EVENT:::", event )
-	
-	const payload = parse( event.body || "" ) as any
-	
-	console.log( payload )
-	
-	// Is it dialog trigger
-	// -> Send back dialog config
-	//    -> Tool checkbox
-	//    -> Mail
-	//    -> Team name
-	
-	
-	// Is it dialog cancellation
-	// -> Don't do shit
-	
-	// Is it dialog submit
-	// -> send dm
-	//    -> "Hey dude, your account has bee created"
-	//    -> gif
-	
-	
-	return Promise.resolve( {
-		statusCode: 200,
-	} )
-}
-
-
-
-//
-// interface MessageActionRequest
-// {
-// 	payload: string
-// }
-//
-// export interface MessageActionRequestPayload
-// {
-// 	type: string
-// 	token: string
-// 	action_ts: string
-// 	team: {
-// 		domain: string
-// 		id: string
-// 	}
-// 	user: {
-// 		name: string
-// 		id: string
-// 	}
-// 	channel: {
-// 		name: string
-// 		id: string
-// 	}
-// 	callback_id: string
-// 	trigger_id: string
-// 	message_ts: string
-// 	message: {
-// 		client_msg_id?: string
-// 		text: string
-// 		type: string
-// 		user?: string
-// 		ts: string
-// 		username?: string
-// 		subtype?: string
-// 		bot_id?: string
-// 	}
-// 	response_url: string
-// }
-//
-// const handler: Handler = ( event: APIGatewayEvent, context: Context, callback: Callback ): Promise<Response> => {
-//
-//
-// 	const params                               = event.queryStringParameters,
-// 	      { payload }: MessageActionRequest    = parse( event.body || "" ) as any,
-// 	      message: MessageActionRequestPayload = JSON.parse( payload )
-//
-// 	console.log( "////////////////////////////////////////" )
-// 	console.log( "PARSED", message )
-// 	console.log( "////////////////////////////////////////" )
-//
-// 	const web = new WebClient( process.env.SLACK_TOKEN )
-//
-//
-// 	return web.auth
-// 		.test()
-// 		.then( ( res: any ) => {
-// 			return res.user_id // Find your user id to know where to send messages to
-// 		} )
-// 		.then( id => {
-// 			console.log( "*********************************************" )
-// 			console.log( "PAYLOAD:::", message.trigger_id )
-// 			console.log( "*********************************************" )
-//
-// 			const promises = [
-// 				web.chat.postMessage( {
-// 					channel: "CGEU070CA",
-// 					text:    `The current time is... now ? 🤷‍♂️`,
-// 				} ),
-// 				web.dialog.open( {
-// 					trigger_id: message.trigger_id,
-// 					dialog:     {
-// 						callback_id:      "ryde-46e2b0",
-// 						title:            `Create account`,
-// 						submit_label:     "Request",
-// 						notify_on_cancel: true,
-// 						state:            "Limo",
-// 						elements:         [
-// 							{
-// 								label:   "Tools",
-// 								type:    "select",
-// 								name:    "meal_preferences",
-// 								options: [
-// 									{
-// 										label: "Github",
-// 										value: "hindu",
-// 									},
-// 									{
-// 										label: "Gitlab",
-// 										value: "vegan",
-// 									},
-// 									{
-// 										label: "Artifactory",
-// 										value: "kosher",
-// 									},
-// 									{
-// 										label: "Vault",
-// 										value: "burrito",
-// 									},
-// 								],
-// 							},
-// 							{
-// 								type:  "text",
-// 								label: "Team name",
-// 								name:  "loc_origin",
-// 							},
-// 						],
-// 					},
-// 				} ),
-// 			]
-// 			return Promise.all( promises )
-// 		} )
-// 		.then( () =>
-// 			({
-// 				headers:    {
-// 					"Content-Type": "application/json",
-// 				},
-// 				statusCode: 200,
-// 				body:       JSON.stringify( {
-// 					message: "Hello 😍",
-// 				} ),
-// 			}) )
-// 		.catch( err => {
-//
-// 			console.log( err )
-// 			return ({
-// 				headers:    {
-// 					"Content-Type": "application/json",
-// 				},
-// 				statusCode: 500,
-// 				body:       JSON.stringify( err ),
-// 			})
-// 		} )
-// }
-
-// const payload: MessageActionRequest = {
-// 	payload: {
-// 		type:         "message_action",
-// 		token:        "KmDISxAGBBgfNVS3f12ILd2a",
-// 		action_ts:    "1550919892.981365",
-// 		team:         {
-// 			id:     "TGGBAA4DU",
-// 			domain: "isthatcenteredgroup",
-// 		},
-// 		user:         {
-// 			id:   "UGESQ31C4",
-// 			name: "e.peninb",
-// 		},
-// 		channel:      {
-// 			id:   "CGEU070CA",
-// 			name: "factory",
-// 		},
-// 		callback_id:  "account_new",
-// 		trigger_id:   "558921874852.560384344470.10ba386f94dda67100c50da433c85949",
-// 		message_ts:   "1550918636.000300",
-// 		message:      {
-// 			type:     "message",
-// 			subtype:  "bot_message",
-// 			text:     "The current time is... now ? :man-shrugging:",
-// 			ts:       "1550918636.000300",
-// 			username: "Random",
-// 			bot_id:   "BGEQKNVNX",
-// 		},
-// 		response_url: "https:\\/\\/hooks.slack.com\\/app\\/TGGBAA4DU\\/558217982864\\/uioZZFM70Dm93kXJirK1j3uM",
-// 	},
-// }
-//
-// const payload2: MessageActionRequest = {
-// 	payload: {
-// 		type:         "message_action",
-// 		token:        "KmDISxAGBBgfNVS3f12ILd2a",
-// 		action_ts:    "1550924311.855212",
-// 		team:         {
-// 			id:     "TGGBAA4DU",
-// 			domain: "isthatcenteredgroup",
-// 		},
-// 		user:         {
-// 			id:   "UGESQ31C4",
-// 			name: "e.peninb",
-// 		},
-// 		channel:      {
-// 			id:   "CGEU070CA",
-// 			name: "factory",
-// 		},
-// 		callback_id:  "account_new",
-// 		trigger_id:   "558941468132.560384344470.3ffaaa71fb62b5f5fd03c1225accaec0",
-// 		message_ts:   "1550924309.000500",
-// 		message:      {
-// 			client_msg_id: "34819af8-f04e-4c56-8f80-96f38a355162",
-// 			type:          "message",
-// 			text:          "lmklj",
-// 			user:          "UGESQ31C4",
-// 			ts:            "1550924309.000500",
-// 		},
-// 		response_url: "https:\\/\\/hooks.slack.com\\/app\\/TGGBAA4DU\\/558985554802\\/iPWhWQI5qrh0BfzZwEkGUL1p",
-// 	},
-// }
-//
-//
-// let t = {
-// 	"type":            "message_action",
-// 	"token":           "KmDISxAGBBgfNVS3f12ILd2a",
-// 	"action_ts":       "1550929511.785663",
-// 	"team":            {
-// 		"id":     "TGGBAA4DU",
-// 		"domain": "isthatcenteredgroup",
-// 	},
-// 	"user":            {
-// 		"id":   "UGESQ31C4",
-// 		"name": "e.peninb",
-// 	},
-// 	"channel":         {
-// 		"id":   "CGEU070CA",
-// 		"name": "factory",
-// 	},
-// 	"callback_id":     "account_new",
-// 	"trigger_id":      "558887688883.560384344470.08cd32f99f38b9ebebe91c6a442521bf",
-// 	"message_ts":      "1550928756.001700",
-// 	"message":         {
-// 		"client_msg_id": "e131f68f-dd54-4375-8945-4006040322c3",
-// 		"type":          "message",
-// 		"text":          "kuh",
-// 		"user":          "UGESQ31C4", "ts": "1550928756.001700",
-// 	}, "response_url": "https:\/\/hooks.slack.com\/app\/TGGBAA4DU\/558965632324\/qW5tATN8hOSjOMdoUB9pHovJ",
-// }
 
 export { handler }
